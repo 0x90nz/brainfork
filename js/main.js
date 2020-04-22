582b969c983346a0c57fb32eae7ffbef322e834f
pb1245 <pbouda14@gmail.com>
2020-04-22 19:04:51 +1200

    Move braces to same line
    JS conventions are hareder to ignore now

--- js/main.js
+++ js/main.js
@@ -3,24 +3,19 @@ var names = {};
 var memory = new Array(1024);
 var ip = 0;
 
-function dumpMem()
-{
-    for(i = 0; i < memory.length - 17; i += 16)
-    {
+function dumpMem() {
+    for(i = 0; i < memory.length - 17; i += 16) {
         var bytestring = "0x" + i.toString(16).padStart(3, "0") + ": ";
-        for(j = i; j < i + 16;j++)
-        {
+        for(j = i; j < i + 16;j++) {
             bytestring += memory[i + j].toString(16).padStart(2, "0") + " ";    
         }
         console.log(bytestring);
     }
 }
 
-function getAddr(name)
-{
+function getAddr(name) {
     // deal with pointers
-    if(name.match(/^\[.+\]$/))
-    {
+    if(name.match(/^\[.+\]$/)) {
         return memory[getAddr(name.substring(1, name.length - 1))];
     }
 
@@ -33,8 +28,7 @@ function getAddr(name)
     return addr;
 }
 
-function getLabelAddr(name)
-{
+function getLabelAddr(name) {
     var addr = parseInt(name);
     if(isNaN(addr))
         addr = labels[name] + 1;
@@ -44,11 +38,9 @@ function getLabelAddr(name)
     return addr;
 }
 
-function executeInstruction(line, output)
-{
+function executeInstruction(line, output) {
     // Ignore line
-    if(line.match(/^[A-z]+\:\s*$/))
-    {
+    if(line.match(/^[A-z]+\:\s*$/)) {
         ip++;
         return true;
     }
@@ -59,8 +51,7 @@ function executeInstruction(line, output)
     if(instruction.length > 1)
         args = instruction[1].split(",");
 
-    if(args != undefined)
-    {
+    if(args != undefined) {
         for(j = 0; j < args.length; j++)
             args[j] = args[j].trim();
     }
@@ -127,8 +118,7 @@ function executeInstruction(line, output)
 
         case "bz":
         case "/0":
-            if(memory[getAddr(args[0])] == 0)
-            {
+            if(memory[getAddr(args[0])] == 0) {
                 ip = getLabelAddr(args[1]);
                 return true;
             }
@@ -136,8 +126,7 @@ function executeInstruction(line, output)
 
         case "bnz":
         case "/!0":
-            if(memory[getAddr(args[0])] != 0)
-            {
+            if(memory[getAddr(args[0])] != 0) {
                 ip = getLabelAddr(args[1]);
                 return true;
             }
@@ -145,8 +134,7 @@ function executeInstruction(line, output)
 
         case "bgt":
         case "/>":
-            if(memory[getAddr(args[0])] > memory[getAddr(args[1])])
-            {
+            if(memory[getAddr(args[0])] > memory[getAddr(args[1])]) {
                 ip = getLabelAddr(args[2]);
                 return true;
             }
@@ -154,8 +142,7 @@ function executeInstruction(line, output)
 
         case "bgteq":
         case "/>=":
-            if(memory[getAddr(args[0])] >= memory[getAddr(args[1])])
-            {
+            if(memory[getAddr(args[0])] >= memory[getAddr(args[1])]) {
                 ip = getLabelAddr(args[2]);
                 return true;
             }
@@ -163,8 +150,7 @@ function executeInstruction(line, output)
 
         case "blt":
         case "/<":
-            if(memory[getAddr(args[0])] < memory[getAddr(args[1])])
-            {
+            if(memory[getAddr(args[0])] < memory[getAddr(args[1])]) {
                 ip = getLabelAddr(args[2]);
                 return true;
             }
@@ -172,8 +158,7 @@ function executeInstruction(line, output)
 
         case "blteq":
         case "/<=":
-            if(memory[getAddr(args[0])] <= memory[getAddr(args[1])])
-            {
+            if(memory[getAddr(args[0])] <= memory[getAddr(args[1])]) {
                 ip = getLabelAddr(args[2]);
                 return true;
             }
@@ -249,17 +234,12 @@ function execute(target, display)
 
     // build names
     lines.forEach(line => {
-        if(line.match(/\.name \d+ \w+$/))
-        {            
+        if(line.match(/\.name \d+ \w+$/)) {            
             names[line.split(" ")[2]] = parseInt(line.split(" ")[1]);
             lines = lines.filter(item => item != line);
-        }
-        else if(line.match(/^#/) || line.match(/^\s*$/))
-        {
+        } else if(line.match(/^#/) || line.match(/^\s*$/)) {
             lines = lines.filter(item => item != line);
-        }
-        else if(line.match(/\.s \d+ \".*\"/))
-        {
+        } else if(line.match(/\.s \d+ \".*\"/)) {
             var str = line.replace(/.s \d+ /, "").replace(/\"/g, "");
             var addr = parseInt(line.split(" ")[1]);
             var i = 0;
@@ -273,32 +253,23 @@ function execute(target, display)
 
             // console.log("String: " + str + " @ " + addr);
             lines = lines.filter(item => item != line);
-        }
-        else if(line.match(/^[A-z]+\:\s*$/))
-        {
+        } else if(line.match(/^[A-z]+\:\s*$/)) {
             labels[line.split(":")[0]] = lines.indexOf(line);
         }
     });
 
-    for(i = 0; i < lines.length; i++)
-    {
+    for(i = 0; i < lines.length; i++) {
         console.log(i + ": " + lines[i]);
     }
 
     timeout = setInterval(function(){
         var output = executeInstruction((lines[ip] == undefined ? "": lines[ip]), outText);
         // console.log("Inst executed: " + lines[ip] + " with result: " + output);
-        if(!output || lines[ip] == undefined)
-        {
+        if(!output || lines[ip] == undefined) {
             console.log("Should stop!");
             clearInterval(timeout);
         }
     }, 1);
-
-    // for(ip = 0; ip < lines.length;)
-    // {
-    //     executeInstruction()
-    // }
 
     console.log("done");
 }
