43fd283bad33eed2a262207311cc32844fb20f72
pb1245 <pbouda14@gmail.com>
2020-04-22 20:01:12 +1200

    Move instruction into a js object
    The old switch statement was getting clumsy

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-let labels;
-let names;
 let memory;
 let ip = 0;
 
 function dumpMem() {
     for(i = 0; i < memory.length - 17; i += 16) {
-        let bytestring = "0x" + i.toString(16).padStart(3, "0") + ": ";
+        let bytestring = '0x' + i.toString(16).padStart(3, '0') + ': ';
         for(j = i; j < i + 16;j++) {
-            bytestring += memory[i + j].toString(16).padStart(2, "0") + " ";    
+            bytestring += memory[i + j].toString(16).padStart(2, '0') + ' ';    
         }
         console.log(bytestring);
     }
@@ -27,13 +25,66 @@ function getAddr(name) {
     return addr;
 }
 
-function getLabelAddr(name) {
-    let addr = parseInt(name);
-    if(isNaN(addr))
-        addr = labels[name] + 1;
-
-    return addr;
+function rawOut(value) {
+    const output = document.getElementById('outputTextArea');
+    output.value += String.fromCharCode(value);
+    output.scrollTop = output.scrollHeight;
 }
+
+let instructions = {
+    'set': {
+        execute: (args, memory, ip) => {
+            memory[getAddr(args[0])] = parseInt(args[1]);
+        }
+    },
+    'inc': {
+        execute: (args, memory, ip) => {
+            memory[getAddr(args[0])]++;
+        }
+    },
+    'dec': {
+        execute: (args, memory, ip) => {
+            memory[getAddr(args[0])]++;
+        }
+    },
+    'out': {
+        execute: (args, memory, ip) => {
+            rawOut(memory[getAddr(args[0])]);
+        }
+    },
+    'mov': {
+        execute: (args, memory, ip) => {
+            memory[getAddr(args[0])] = memory[getAddr(args[1])];
+        }
+    },
+    'add': {
+        execute: (args, memory, ip) => {
+            memory[getAddr(args[0])] += memory[getAddr(args[1])];
+        }
+    },
+    'sub': {
+        execute: (args, memory, ip) => {
+            memory[getAddr(args[0])] -= memory[getAddr(args[1])];
+        }
+    },
+    'loop': {
+        execute: (args, memory, ip) => {
+            if(memory[getAddr(args[0])] == 0) {
+                ip = getLabelAddr(args[1]);
+            }
+        }
+    },
+    aliases: {
+        's': 'set',
+        'o': 'out',
+        'm': 'mov',
+        'a': 'add',
+        'u': 'sub',
+        'l': 'loop',
+        'i': 'inc',
+        'd': 'dec'
+    }
+};
 
 function executeInstruction(line, output) {
     // Ignore line
@@ -46,7 +97,7 @@ function executeInstruction(line, output) {
     const name = instruction[0];
     let args;
     if(instruction.length > 1)
-        args = instruction[1].split(",");
+        args = instruction[1].split(',');
 
     if(args != undefined) {
         for(j = 0; j < args.length; j++)
@@ -55,212 +106,106 @@ function executeInstruction(line, output) {
 
     const debug = document.getElementById('debugTextArea');
 
-    debug.innerHTML = "";
+    debug.innerHTML = '';
 
-    debug.innerHTML += "Instruction: " + name + "\n";
+    debug.innerHTML += 'Instruction: ' + name + '\n';
     if(args != undefined)
-        debug.innerHTML += "Arguments: " + args.join(', ') + "\n";
+        debug.innerHTML += 'Arguments: ' + args.join(', ') + '\n';
 
-    switch (name) {
-        case "~":
-        case "j":
-            ip = getLabelAddr(args[0]);
-            console.log("jumping to " + args[0] + " (" + ip + ")");
-            return true;
-        
-        case "set":
-        case "=":
-            memory[getAddr(args[0])] = parseInt(args[1]);
-            console.log(args[1]);
-            break;
+    const operation = 
+        instructions[name] === undefined ?
+        instructions[instructions.aliases[name]] : 
+        instructions[name];
 
-        case "out":
-        case "<":    
-            output.value += memory[getAddr(args[0])] + "\n";
-            output.scrollTop = output.scrollHeight;
-            console.log(memory[getAddr(args[0])]);
-            break;
-
-        case "outasc":
-        case "<*":    
-                output.value += String.fromCharCode(memory[getAddr(args[0])]);
-                output.scrollTop = output.scrollHeight;
-                break;
-
-        case "move":
-        case "->":
-            console.log("moving from: " +  getAddr(args[1]) + " to: " + getAddr(args[0]));
-            memory[getAddr(args[0])] = memory[getAddr(args[1])];
-            break; 
-
-        case "add":
-        case "+":
-            memory[getAddr(args[0])] += memory[getAddr(args[1])];
-            break;
-
-        case "addi":
-        case "+.":
-            memory[getAddr(args[0])] += parseInt(args[1]);
-            break;
-
-        case "sub":
-        case "-":
-            memory[getAddr(args[0])] -= memory[getAddr(args[1])];
-            break;
-
-        case "subi":
-        case "-.":
-            memory[getAddr(args[0])] -= parseInt(args[1]);
-            break;
-
-        case "bz":
-        case "/0":
-            if(memory[getAddr(args[0])] == 0) {
-                ip = getLabelAddr(args[1]);
-                return true;
-            }
-            break;
-
-        case "bnz":
-        case "/!0":
-            if(memory[getAddr(args[0])] != 0) {
-                ip = getLabelAddr(args[1]);
-                return true;
-            }
-            break;
-
-        case "bgt":
-        case "/>":
-            if(memory[getAddr(args[0])] > memory[getAddr(args[1])]) {
-                ip = getLabelAddr(args[2]);
-                return true;
-            }
-            break;
-
-        case "bgteq":
-        case "/>=":
-            if(memory[getAddr(args[0])] >= memory[getAddr(args[1])]) {
-                ip = getLabelAddr(args[2]);
-                return true;
-            }
-            break;
-
-        case "blt":
-        case "/<":
-            if(memory[getAddr(args[0])] < memory[getAddr(args[1])]) {
-                ip = getLabelAddr(args[2]);
-                return true;
-            }
-            break;
-
-        case "blteq":
-        case "/<=":
-            if(memory[getAddr(args[0])] <= memory[getAddr(args[1])]) {
-                ip = getLabelAddr(args[2]);
-                return true;
-            }
-            break;
-
-        case "h":
-        case "hlt":
-        case "halt":
-            return false;
-
-        case "dump":
-            dumpMem(memory);
-            break;
-        
-        default:
-            console.log("What's this? " + line);
-            debug.innerHTML += "Unknown line: " + line;
-            return false;
+    if(operation !== undefined) {
+        operation.execute(args, memory, ip);
+    } else {
+        debug.innerHTML = 'Undefined instruction: ' + name;
+        return false;
     }
+
     ip++;
     return true;
 }
 
-function minify(target)
-{
-    const elem = document.getElementById(target);
-    const program = elem.value;
-    let outProgram = "";
+// function minify(target)
+// {
+//     const elem = document.getElementById(target);
+//     const program = elem.value;
+//     let outProgram = '';
 
-    const convert = {"j":"~", "set":"=", "out":"<", "outasc":"<*", "move":"->", "add":"+",
-        "addi":"+.", "sub":"-", "subi":"-.", "bz":"/0", "bnz":"/!0", "bgt":"/>", "bgteq":"/>=",
-        "blt":"/<", "blteq":"/<=", "halt":"h"};
+//     const convert = {'j':'~', 'set':'=', 'out':'<', 'outasc':'<*', 'move':'->', 'add':'+',
+//         'addi':'+.', 'sub':'-', 'subi':'-.', 'bz':'/0', 'bnz':'/!0', 'bgt':'/>', 'bgteq':'/>=',
+//         'blt':'/<', 'blteq':'/<=', 'halt':'h'};
 
-    let names = {};
+//     let names = {};
 
-    program.split("\n").forEach(line => {
-        const instruction = line.split(/ (.+)/)[0].trim();
-        const args = line.split(/ (.+)/)[1];
+//     program.split('\n').forEach(line => {
+//         const instruction = line.split(/ (.+)/)[0].trim();
+//         const args = line.split(/ (.+)/)[1];
 
-        console.log(convert[instruction]);
+//         console.log(convert[instruction]);
 
-        if(convert[instruction] != undefined)
-            instruction = convert[instruction];
+//         if(convert[instruction] != undefined)
+//             instruction = convert[instruction];
 
-        if(args == undefined)
-            args = "";
+//         if(args == undefined)
+//             args = '';
 
-        if(instruction != "")
-            outProgram += instruction + " " + args + ";";
-    });
+//         if(instruction != '')
+//             outProgram += instruction + ' ' + args + ';';
+//     });
 
-    elem.value = outProgram;
-}
+//     elem.value = outProgram;
+// }
 
-function execute(target, display)
-{
+function runProgram(target, display) {
     console.clear();
 
     const outText = document.getElementById(display);
-    outText.value = ""; // clear for prev runs
+    outText.value = ''; // clear for prev runs
 
     const program = document.getElementById(target).value;
     let lines = program.split(/[\n|;]/);
-    console.log(lines);
 
-    memory = new Array(4096);
+    memory = new Array(512);
     for(i = 0; i < memory.length; i++)
         memory[i] = 0; // fill memory
 
-    labels = {};
-    names = {};
     ip = 0;
 
-    // build names
-    lines.forEach(line => {
-        if(line.match(/\.name \d+ \w+$/)) {            
-            names[line.split(" ")[2]] = parseInt(line.split(" ")[1]);
-            lines = lines.filter(item => item != line);
-        } else if(line.match(/^#/) || line.match(/^\s*$/)) {
-            lines = lines.filter(item => item != line);
-        } else if(line.match(/\.s \d+ \".*\"/)) {
-            const str = line.replace(/.s \d+ /, "").replace(/\"/g, "");
-            const addr = parseInt(line.split(" ")[1]);
-            let i = 0;
-            for( ; i < str.length; i++) {
-                memory[i+addr] = str.charCodeAt(i);
-            }
+    // // build names
+    // lines.forEach(line => {
+    //     if(line.match(/\.name \d+ \w+$/)) {            
+    //         names[line.split(' ')[2]] = parseInt(line.split(' ')[1]);
+    //         lines = lines.filter(item => item != line);
+    //     } else if(line.match(/^#/) || line.match(/^\s*$/)) {
+    //         lines = lines.filter(item => item != line);
+    //     } else if(line.match(/\.s \d+ \'.*\'/)) {
+    //         const str = line.replace(/.s \d+ /, '').replace(/\'/g, '');
+    //         const addr = parseInt(line.split(' ')[1]);
+    //         let i = 0;
+    //         for( ; i < str.length; i++) {
+    //             memory[i+addr] = str.charCodeAt(i);
+    //         }
             
-            memory[i+addr+1] = 0;
+    //         memory[i+addr+1] = 0;
 
-            // console.log("String: " + str + " @ " + addr);
-            lines = lines.filter(item => item != line);
-        } else if(line.match(/^[A-z]+\:\s*$/)) {
-            labels[line.split(":")[0]] = lines.indexOf(line);
-        }
-    });
+    //         // console.log('String: ' + str + ' @ ' + addr);
+    //         lines = lines.filter(item => item != line);
+    //     } else if(line.match(/^[A-z]+\:\s*$/)) {
+    //         labels[line.split(':')[0]] = lines.indexOf(line);
+    //     }
+    // });
 
     timeout = setInterval(function(){
-        const output = executeInstruction((lines[ip] == undefined ? "": lines[ip]), outText);
+        const output = executeInstruction((lines[ip] == undefined ? '': lines[ip]), outText);
 
         if(!output || lines[ip] == undefined) {
-            console.log("Should stop!");
+            console.log('Should stop!');
             clearInterval(timeout);
         }
-    }, parseInt(document.getElementById("executionDelayMs").value));
+    }, parseInt(document.getElementById('executionDelayMs').value));
 
-    console.log("done");
+    console.log('done');
 }
